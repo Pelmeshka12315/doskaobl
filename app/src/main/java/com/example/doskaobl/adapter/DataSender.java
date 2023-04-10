cc19deebdcca5dc24b89e3e87e3e7aad44c37e64
User <https://github.com/DanilRossov/AutoRossov.git>
2023-04-10 18:35:27 +0900

    Initial commit

---
+++ app/src/main/java/com/example/doskaobl/adapter/DataSender.java
@@ -0,0 +1,10 @@
+package com.example.doskaobl.adapter;
+
+import com.example.doskaobl.NewPost;
+
+import java.util.List;
+
+public interface DataSender {
+
+    public void onDataRecived(List<NewPost> listData);
+}
